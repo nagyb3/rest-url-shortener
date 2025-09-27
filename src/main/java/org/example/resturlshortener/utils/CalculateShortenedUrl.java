0beb47538a9f3ba3895bb958dff2599df4db7cda
Bence Nagy <nagybence2003@gmail.com>
2025-09-27 13:15:30 +0200

    Update CalculateShortenedUrl.java

--- src/main/java/org/example/resturlshortener/utils/CalculateShortenedUrl.java
+++ src/main/java/org/example/resturlshortener/utils/CalculateShortenedUrl.java
@@ -1,9 +1,12 @@
 package org.example.resturlshortener.utils;
 
 import org.example.resturlshortener.entities.UrlShort;
+import org.springframework.beans.factory.annotation.Value;
 
 public class CalculateShortenedUrl {
-    String apiBaseUrl = "http://localhost:8080";
+
+    @Value("${API_BASE_URL:http://localhost:8080}")
+    private String apiBaseUrl;
 
     public String getShortenedUrl(UrlShort urlShort) {
         return apiBaseUrl + "/" + urlShort.getId();
